69465630a65f6ee6673c202e41773cc791f83573
Gabriel Miranda <gabrielmiranda61637@gmail.com>
2025-04-06 12:45:29 -0300

    Spring Initialzr

--- README.md
+++
@@ -1,2 +0,0 @@
-# AgendamentoAPI
-A API AgendamentoAPI é uma ferramenta que possibilita o desenvolver utilizar os endpoints para integrar o agendamento a parte da sua aplicação
---
+++ src/test/java/com/AgendamentoAPI/AgendamentoAPI/AgendamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgendamentoAPI.AgendamentoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
